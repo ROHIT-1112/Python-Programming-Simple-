d03935ac43c3b27d88415c3f38e9ac5a67bd991f
ROHIT CHATURVEDI <80148163+ROHIT-1112@users.noreply.github.com>
2021-03-09 23:23:38 +0530

    Add files via upload

---
+++ While Loops - 1.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Python Codes
+
+# In[5]:
+
+
+# While Loops in Python .
+while True:
+    line = input('>')
+    if line == 'done':
+        break
+    print(line)
+print('Result Achieved')
+
+
+# In[24]:
+
+
+# Operator Info & Prediction .
+# Program for User to know in which year , He/She will turn 100 Years .
+# Please Enter your Info .
+name = input("Enter Your Name :- ").upper()
+age = int(input("Enter Your Age :- "))
+current_year = 2021
+year = current_year + 100
+print('{},you will turn 100 years in {} .'.format(name,year))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
