5f5fc50278db0729a6c87df8720c40d8c2909602
ROHIT CHATURVEDI <80148163+ROHIT-1112@users.noreply.github.com>
2021-03-10 20:06:59 +0530

    Add files via upload

---
+++ Counting_in_For_Loops.py
@@ -0,0 +1,8 @@
+# Counting in Loop in Python
+count = 0
+cars = ['alto','kia','xcent','swift','i10','defender','fortuner','Xuv 500','Scorpio']
+for car in cars :
+    count = count + 1 # Counting Loop value .
+    print('Current car :- {} , Number of times loop executed :- {} .'.format(car,count))
+print("\nTotal times Loop Executed :- {}.".format(count))
+print("Loop Finished !!!! Process Over !!!!")
