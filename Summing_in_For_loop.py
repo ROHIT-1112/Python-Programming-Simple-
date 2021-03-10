5b0c711430ac358ef42e16c1ad13cec316875daa
ROHIT CHATURVEDI <80148163+ROHIT-1112@users.noreply.github.com>
2021-03-10 20:07:20 +0530

    Add files via upload

---
+++ Summing_in_For_loop.py
@@ -0,0 +1,8 @@
+
+# Summing in a loop in Python .
+summ = 0
+value = (100,181,12,34,50,120,55,30)
+for val in value:
+    summ = summ + val
+    print("Sum Value :- {}.".format(summ))
+print("\n\nTotal Sum Value :- {}.".format(summ))
